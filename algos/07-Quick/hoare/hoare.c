48276e21c14f7fe1141412712e59acc59e333ddf
luisandreiouano <110468848+LowisWano@users.noreply.github.com>
2025-02-26 22:58:10 +0800

    feat: hoare review

--- algos/07-Quick/hoare/hoare.c
+++ algos/07-Quick/hoare/hoare.c
@@ -1,52 +1,49 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int hoare_partition(int arr[], int start, int end) {
-    int pivot = arr[start];
-    int i = start;
-    int j = end;
-    
-    while (i < j) {
-        // Move i forward while element is less than pivot
-        for (; arr[i] < pivot && i < end; i++);
-  
-        // Move j backward while element is greater than pivot
-        for (; arr[j] > pivot && j > start; j--);
-  
-        
-        // If pointers haven't crossed, swap elements
-        if (i < j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-    return j;
+int hoare_partition(int arr[], int start, int end);
+void quicksort(int arr[], int start, int end);
+
+int main(void){
+  int arr[] = {12, 34, 54, 2, 3, 5, 7, 23, 1, 26};
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  quicksort(arr, 0, n-1);
+
+  for(int i=0; i<n; i++){
+    printf("%d ", arr[i]);
+  }
+
+  return 0;
 }
 
-void quickSort(int arr[], int start, int end) {
-    if (start < end) {
-        int p = hoare_partition(arr, start, end);
-        quickSort(arr, start, p);
-        quickSort(arr, p + 1, end);
+int hoare_partition(int arr[], int start, int end){
+  int i = start;
+  int j = end;
+  int pivot = arr[start];
+
+  while(i<j){
+    while(i<end && arr[i] < pivot){
+      i++;
     }
+
+    while(j>=0 && arr[j] > pivot){
+      j--;
+    }
+
+    if(i<j){
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  
+  return j;
 }
 
-
-int main(void) {
-    int arr[] = {3, 1, 4, 2 , 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    printf("Original array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    
-    quickSort(arr, 0, n-1);
-    
-    printf("\nSorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    
-    return 0;
+void quicksort(int arr[], int start, int end){
+  if(start<end){
+    int i = hoare_partition(arr, start, end);
+    quicksort(arr, start, i);
+    quicksort(arr, i+1, end);
+  }
 }
