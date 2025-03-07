adc61928baa1e9e1d20d703067abd182cf74dea2
luisandreiouano <110468848+LowisWano@users.noreply.github.com>
2025-03-07 08:42:44 +0800

    feat: hoare

--- algos/sorting/Quick/Hoare/hoare.c
+++ algos/sorting/Quick/Hoare/hoare.c
@@ -1,5 +1,5 @@
-#include "../../utilities/general.h"
-#include "../../utilities/test.h"
+#include "../../../utilities/general.h"
+#include "../../../utilities/test.h"
 
 int hoare_partition(int arr[], int start, int end);
 void quicksort(int arr[], int start, int end);
@@ -11,9 +11,34 @@ int main(void){
 }
 
 void quicksort(int arr[], int start, int end){
-
+  if(start < end){
+    int p = hoare_partition(arr, start, end);
+    quicksort(arr, start, p);
+    quicksort(arr, p+1, end);
+  }
 }
 
 int hoare_partition(int arr[], int start, int end){
- 
+  int pivot = arr[start];
+  int i = start - 1, j = end + 1;
+  
+  while(i<j){
+    // more intuitive to use do while loops for hoare's to guarantee movement to avoid i and j getting stuck in edge cases where they both encounter a value same as pivot
+    do{
+      i++;
+    }while(i<=end && arr[i]<pivot);
+
+    do{
+      j--;
+    }while(j>=start && arr[j]>pivot);
+    
+    if(i<j){
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+
+  }
+
+  return j;
 }
